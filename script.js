cae57d30473a913a4a751ea6a3111cf8d064bc99
LuuizPaes <lu.fernando.paes@hotmail.com>
2024-05-30 19:15:57 -0300

    Initial commit of the project

---
+++ script.js
@@ -0,0 +1,39 @@
+const timerEl = document.querySelector(".timer");
+const MarksList = document.querySelector(".marks-list");
+let intervalId = 0;
+let timer = 0;
+let marks = [];
+
+const formatTime = (time) => {
+    const hours = Math.floor(time / 360000);
+    const minutes = Math.floor((time % 360000) / 6000);
+    const seconds = Math.floor((time % 6000) / 100);
+    const hundredths = time % 100;
+
+  return `${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}:${hundredths.toString().padStart(2, "0")}:`;
+}
+
+const toggleTimer = () => {
+  const button = document.querySelector(".power");
+  const action = button.getAttribute("action");
+
+  clearInterval(intervalId);
+
+  if (action == "start" || action == "continue") {
+    intervalId = setInterval(() => {
+      timer += 1;
+      setTimer(timer);
+    }, 10);
+    button.setAttribute("action", "pause");
+    button.innerHTML = '<i class="fa-solid fa-pause"></i>';
+  } else if (action == "pause") {
+    button.setAttribute("action", "continue");
+    button.innerHTML = '<i class="fa-solid fa-play"></i>';
+  }
+}
+
+const setTimer = (time) => {
+  timerEl.innerText = formatTime(time);
+}
+
+document.querySelector(".power").addEventListener("click", toggleTimer);
